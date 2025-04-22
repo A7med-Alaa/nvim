e79013c7bbdbbfddd993c358b5bf32b1c2cf6f7a
Ahmed Alaa <88171283+A7med-Alaa@users.noreply.github.com>
2025-04-22 16:28:49 +0200

    Update lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -15,8 +15,6 @@ return {
           "clangd",
           "jdtls",
           "cmake",
-          "powershell_es",
-          "glsl_analyzer",
           "prismals",
           "jsonls",
           "html",
@@ -39,7 +37,6 @@ return {
       local lspconf = require("lspconfig")
       local navic = require("nvim-navic")
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local bin_path = "C:/Users/hp/AppData/Local/nvim-data/mason/bin/"
       local on_attach = function(client, bufnr)
         if client.server_capabilities.documentSymbolProvider then
           navic.attach(client, bufnr)
@@ -49,12 +46,10 @@ return {
       lspconf.pylsp.setup({
         on_attach = on_attach,
         capabilities = capabilities,
-        cmd = { bin_path .. "pylsp.cmd" },
       })
       lspconf.lua_ls.setup({
         on_attach = on_attach,
         capabilities = capabilities,
-        cmd = { bin_path .. "lua-language-server.cmd" },
       })
       lspconf.clangd.setup({
         on_attach = on_attach,
@@ -63,52 +58,42 @@ return {
       lspconf.cmake.setup({
         on_attach = on_attach,
         capabilities = capabilities,
-        cmd = { bin_path .. "cmake-language-server.cmd" },
       })
       lspconf.tailwindcss.setup({
         on_attach = on_attach,
         capabilities = capabilities,
-        cmd = { bin_path .. "tailwindcss-language-server.cmd" },
       })
       lspconf.glsl_analyzer.setup({
         on_attach = on_attach,
         capabilities = capabilities,
-        cmd = { bin_path .. "glsl_analyzer.cmd" },
       })
       lspconf.prismals.setup({
         on_attach = on_attach,
         capabilities = capabilities,
-        cmd = { bin_path .. "prisma-language-server.cmd" },
       })
       lspconf.jsonls.setup({
         on_attach = on_attach,
         capabilities = capabilities,
-        cmd = { bin_path .. "vscode-json-language-server.cmd" },
       })
       lspconf.html.setup({
         on_attach = on_attach,
         capabilities = capabilities,
-        cmd = { bin_path .. "vscode-html-language-server.cmd" },
       })
       lspconf.cssls.setup({
         on_attach = on_attach,
         capabilities = capabilities,
-        cmd = { bin_path .. "vscode-css-language-server.cmd" },
       })
       lspconf.ts_ls.setup({
         on_attach = on_attach,
         capabilities = capabilities,
-        cmd = { bin_path .. "typescript-language-server.cmd", "--stdio" },
       })
       lspconf.jdtls.setup({
         on_attach = on_attach,
         capabilities = capabilities,
-        cmd = { bin_path .. "jdtls.cmd" },
       })
       lspconf.powershell_es.setup({
         on_attach = on_attach,
         capabilities = capabilities,
-        cmd = { bin_path .. "powershell-editor-services.cmd" },
       })
 
       -- vim.diagnostic.config({
