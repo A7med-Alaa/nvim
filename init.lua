f6cc989fa0f758ae26ca1eb5c5a59872192d173e
Ahmed Alaa <ahmedalaa.developer1@gmail.com>
2025-04-25 01:13:20 +0300

    Added a new plugin for git diff

--- init.lua
+++ init.lua
@@ -52,9 +52,8 @@ vim.keymap.set("v", "<", "<gv")
 --Buffers and Tabs
 vim.keymap.set("n", "<leader>bd", ":bd!<CR>")
 vim.keymap.set("n", "<leader>tn", ":tabnext<CR>")
-vim.keymap.set("n", "<leader>tc", ":tabnew<CR>")
+vim.keymap.set("n", "<leader>tc", ":tabclose<CR>")
 vim.keymap.set("n", "<leader>tp", ":tabprevious<CR>")
-vim.keymap.set("n", "<leader>td", ":tabclose<CR>")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<left><left><left>]])
 --
--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -5,8 +5,24 @@ return {
 			require("gitsigns").setup({})
 
 			vim.keymap.set("n", "<leader>gp", "<CMD>Gitsigns preview_hunk<CR>")
+			-- vim.keymap.set("n", "<leader>gdf", function()
+			-- 	-- local relative_path = vim.fn.expand("%")
+			-- 	--     local filename = vim.fn.expand("%:t")
+			-- 	-- vim.cmd("tab Git diff " .. relative_path .. " | file Git Diff " .. filename)
+			--      vim.cmd("Gitsigns diffthis")
+			-- end, { desc = "Git Diff of Current File relative to the Index" })
 		end,
 	},
+  {
+    "sindrets/diffview.nvim",
+    config = function()
+      require("diffview").setup()
+
+			vim.keymap.set("n", "<leader>gd", function()
+				vim.cmd("DiffviewOpen")
+			end, { desc = "Git Diff of All Files relative to the Index" })
+    end,
+  },
 	{
 		"tpope/vim-fugitive",
 		config = function()
@@ -17,17 +33,6 @@ return {
 			vim.keymap.set("n", "<leader>gl", function()
 				vim.cmd("tab Git log | file Git Log")
 			end, { desc = "Git Log" })
-
-			vim.keymap.set("n", "<leader>gda", function()
-				vim.cmd("tab Git diff | file Git Diff")
-			end, { desc = "Git Diff of All Files relative to the Index" })
-
-			vim.keymap.set("n", "<leader>gdf", function()
-				-- local relative_path = vim.fn.expand("%")
-				--     local filename = vim.fn.expand("%:t")
-				-- vim.cmd("tab Git diff " .. relative_path .. " | file Git Diff " .. filename)
-        vim.cmd("Gitsigns diffthis")
-			end, { desc = "Git Diff of Current File relative to the Index" })
 
 			vim.keymap.set("n", "<leader>gh", function()
 				vim.cmd("tab Git show HEAD | file Git Show HEAD")
